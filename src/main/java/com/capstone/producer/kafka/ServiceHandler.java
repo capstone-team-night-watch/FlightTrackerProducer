8892af3c524e7d1fc38579a43d726758546e38b5
Derek <dheimes@unomaha.edu>
2023-04-02 12:53:59 -0500

    Added some stuff

--- src/main/java/com/capstone/producer/kafka/ServiceHandler.java
+++ src/main/java/com/capstone/producer/kafka/ServiceHandler.java
@@ -2,6 +2,7 @@ package com.capstone.producer.kafka;
 
 import com.capstone.producer.clients.AviationStackClientCaller;
 import com.capstone.producer.common.bindings.FlightInfo;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,16 +28,32 @@ public class ServiceHandler {
         //Validate request?
 
         // Gets a FlightInfo Object from client
-        //FlightInfo flightResp = aviationStackClientCaller.getFlightByIcao(flight_icao);
-        //String flightRespStr = flightResp.toString();
+        FlightInfo flightResp = aviationStackClientCaller.getFlightByIcao(flight_icao);
+        String flightRespStr = flightResp.toString();
 
-        List<FlightInfo> flightInfos = aviationStackClientCaller.getAllActiveFlightsWithLive();
 
         //Push response to topic?
+        // Commented out due to Kafka service not running
         //RecordMetadata metadata = KafkaProducerExample.runProducer(flightRespStr);
         //LOGGER.debug("Metadata: {}", metadata.toString());
 
         // Return toString of FlightInfo Object to user on page
+        return flightRespStr;
+    }
+
+    public String handleLiveFlights() {
+
+
+        //Validate request?
+
+        // Gets a list of FlightInfo Objects from client
+        List<FlightInfo> flightInfos = aviationStackClientCaller.getAllActiveFlightsWithLive();
+
+        // Commented out due to Kafka service not running
+        //RecordMetadata metadata = KafkaProducerExample.runProducer(flightRespStr);
+        //LOGGER.debug("Metadata: {}", metadata.toString());
+
+        // Return toStrings of all FlightInfo Objects to user on page
         return flightInfos.stream().map(FlightInfo::toString).collect(Collectors.joining("\n\n"));
     }
 
