016427fcaf7cc9b95e1b2964a8e3591684a948dc
Derek <dheimes@unomaha.edu>
2023-04-13 18:53:44 -0500

    Socket Setup

--- src/main/java/com/capstone/producer/controller/ProducerController.java
+++ src/main/java/com/capstone/producer/controller/ProducerController.java
@@ -1,16 +1,19 @@
 package com.capstone.producer.controller;
 
-import com.capstone.producer.kafka.KafkaProducerExample;
 import com.capstone.producer.kafka.ServiceHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.apache.kafka.clients.producer.RecordMetadata;
-import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 
 @RestController
 public class ProducerController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProducerController.class);
 
     private final ServiceHandler serviceHandler;
     public ProducerController(ServiceHandler serviceHandler) {
@@ -22,11 +25,28 @@ public class ProducerController {
             produces = {MediaType.APPLICATION_JSON_VALUE}
     )
     @ResponseBody
+    @CrossOrigin("*")
     public String controller(@PathVariable("icao") String message) throws JsonProcessingException {
         //RecordMetadata metadata = KafkaProducerExample.runProducer(message);
         //return "Message Sent to Kafka Broker was: " + message + "\n" + "MetaData from send topic: " + metadata.topic() + " Partition: " + metadata.partition() + " Timestamp: " + metadata.timestamp() + " Offset: "+ metadata.offset();
 
-        return serviceHandler.handle(message);
+        LOGGER.info("Received request with icao: {}", message);
+
+        return serviceHandler.handleFlightIcao(message);
+    }
+
+    @RequestMapping(
+            path = "/flighticao/getLive",
+            method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    @ResponseBody
+    @CrossOrigin("*")
+    public String controller() throws JsonProcessingException {
+        //RecordMetadata metadata = KafkaProducerExample.runProducer(message);
+        //return "Message Sent to Kafka Broker was: " + message + "\n" + "MetaData from send topic: " + metadata.topic() + " Partition: " + metadata.partition() + " Timestamp: " + metadata.timestamp() + " Offset: "+ metadata.offset();
+
+        return serviceHandler.handleLiveRequest();
     }
 
     @RequestMapping(
