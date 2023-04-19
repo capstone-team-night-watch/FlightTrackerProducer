313a19d71d8b7b963d218c6c8d039955a6a4ceee
Justin Kaczmarek <justinkaczmarek@unomaha.edu>
2023-04-18 20:26:11 -0500

    add more tests

---
+++ src/test/java/com/capstone/producer/common/bindings/LiveTest.java
@@ -0,0 +1,86 @@
+package com.capstone.producer.common.bindings;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LiveTest {
+
+    private Live live;
+
+    @Before
+    public void setUp(){
+        live = new Live();
+    }
+
+    @Test
+    public void getAndSetUpdated(){
+        live.setUpdated("UPDATED");
+        String result = live.getUpdated();
+
+        assertEquals("UPDATED", result);
+    }
+
+    @Test
+    public void getAndSetLatitude(){
+        live.setLatitude(1);
+        float result = live.getLatitude();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void getAndSetLongitude(){
+        live.setLongitude(1);
+        float result = live.getLongitude();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void getAndSetAltitude(){
+        live.setAltitude(1);
+        int result = live.getAltitude();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void getAndSetDirection(){
+        live.setDirection(1);
+        int result = live.getDirection();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void getAndSetSpeed_horizontal(){
+        live.setSpeed_horizontal(1);
+        float result = live.getSpeed_horizontal();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void getAndSetSpeed_vertical(){
+        live.setSpeed_vertical(1);
+        float result = live.getSpeed_vertical();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void getAndSetIs_ground(){
+        live.setIs_ground(true);
+        boolean result = live.isIs_ground();
+
+        assertEquals(true, result);
+    }
+
+
+}
--- src/test/java/com/capstone/producer/kafka/ServiceHandlerTest.java
+++ src/test/java/com/capstone/producer/kafka/ServiceHandlerTest.java
@@ -1,4 +1,65 @@
 package com.capstone.producer.kafka;
 
+import com.capstone.producer.clients.AviationStackClientCaller;
+import com.capstone.producer.common.bindings.Flight;
+import com.capstone.producer.common.bindings.FlightInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ServiceHandlerTest {
+
+    @Mock
+    private AviationStackClientCaller caller;
+
+    @InjectMocks
+    private ServiceHandler serviceHandler;
+
+    private final static ObjectMapper om = new ObjectMapper();
+
+    private static JsonNode JSON_NODE;
+
+    static {
+        try {
+            JSON_NODE = om.readTree("{ \"field1\" : \"Fee\", \"field2\" : \"Fi\"}");
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void handleFlightIcao() throws JsonProcessingException {
+        when(caller.getFlightByIcao(anyString())).thenReturn(JSON_NODE);
+
+        String result = serviceHandler.handleFlightIcao("JD123");
+
+        assertEquals("{\"field1\":\"Fee\",\"field2\":\"Fi\"}", result);
+    }
+
+    @Test
+    public void handLiveRequest() throws JsonProcessingException {
+        Flight flight = new Flight();
+        flight.setIcao("ICAO");
+
+        FlightInfo flightInfo = new FlightInfo();
+        flightInfo.setFlight(flight);
+        when(caller.getAllActiveFlightsWithLive()).thenReturn(Arrays.asList(flightInfo));
+
+        String result = serviceHandler.handleLiveRequest();
+
+        assertEquals("{\"icaos\":\"ICAO\"}", result);
+    }
+
 }
