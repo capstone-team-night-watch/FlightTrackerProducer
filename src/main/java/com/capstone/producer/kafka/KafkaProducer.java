fcc98034df96b00eb238f428212cbfe4d93cf7c3
ewags2 <114793424+ewags2@users.noreply.github.com>
2024-02-18 23:04:37 -0600

    Last code smell for the night

--- src/main/java/com/capstone/producer/kafka/KafkaProducer.java
+++ src/main/java/com/capstone/producer/kafka/KafkaProducer.java
@@ -8,7 +8,6 @@ import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
