da6bd20336e01cedd8349f4f7e386e238e1f45d7
ewags2 <114793424+ewags2@users.noreply.github.com>
2024-02-18 22:35:14 -0600

    Sonar fixes

--- src/test/java/com/capstone/producer/controller/GetInfoControllerTest.java
+++ src/test/java/com/capstone/producer/controller/GetInfoControllerTest.java
@@ -10,7 +10,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
