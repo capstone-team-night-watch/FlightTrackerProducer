98ef425f0ee775f2cc2b124eb4ed7368144cd06e
Komlan Kodoh <komlankodoh@gmail.com>
2024-02-21 21:08:39 -0600

    Update producer id

--- src/main/java/com/capstone/producer/kafka/KafkaProducer.java
+++ src/main/java/com/capstone/producer/kafka/KafkaProducer.java
@@ -31,7 +31,7 @@ public class KafkaProducer {
     private static Producer<Long, String> createProducer() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer-TESTER");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "flight-tracker-producer");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
