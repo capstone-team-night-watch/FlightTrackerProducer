bb2e359889a73978e030d0e3a7919f709cf83d48
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2024-03-06 11:29:14 -0600

    added minAltitude and maxAltitude properties to TfrNotam

--- src/main/java/com/capstone/producer/common/bindings/TfrNotam.java
+++ src/main/java/com/capstone/producer/common/bindings/TfrNotam.java
@@ -20,6 +20,10 @@ public class TfrNotam {
     // Radius is in meters
     private double radius;
 
+    private double minAltitude;
+
+    private double maxAltitude;
+
     private String startTime;
 
     private String endTime;
@@ -29,11 +33,13 @@ public class TfrNotam {
         super();
     }
 
-    public TfrNotam(String notamNumber, String notamType, List<Double> latlong, double radius, String startTime, String endTime){
+    public TfrNotam(String notamNumber, String notamType, List<Double> latlong, double radius, double minAltitude, double maxAltitude, String startTime, String endTime){
         this.notamNumber = notamNumber;
         this.notamType = notamType;
         this.latlong = latlong;
         this.radius = radius;
+        this.minAltitude = minAltitude;
+        this.maxAltitude = maxAltitude;
         this.startTime = startTime;
         this.endTime = endTime;
     }
