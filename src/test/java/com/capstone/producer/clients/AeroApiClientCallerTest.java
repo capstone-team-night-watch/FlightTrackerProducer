8d8f1932e8a5c20f7d3ff87720787e12295eb6f7
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2024-03-19 21:25:20 -0500

    created tests for AeroApiClientCaller

--- src/test/java/com/capstone/producer/clients/AeroApiClientCallerTest.java
+++ src/test/java/com/capstone/producer/clients/AeroApiClientCallerTest.java
@@ -2,7 +2,9 @@ package com.capstone.producer.clients;
 
 import com.capstone.producer.common.bindings.aero.FlightInfoFaid;
 import com.capstone.producer.common.bindings.aero.Operator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.InputStream;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,9 +33,23 @@ public class AeroApiClientCallerTest {
 
     private AeroApiClientCaller aeroApiClientCaller;
 
+    private static JsonNode jsonNode;
+
+    private FlightInfoFaid FLIGHT_INFO_FAID;
+
     @Before
     public void setUp() {
         aeroApiClientCaller = new AeroApiClientCaller(client, BASE_URL, "key");
+
+        // read in json flight data
+        try(InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("flight_data.json")){
+            ObjectMapper mapper = new ObjectMapper();
+            jsonNode = mapper.readValue(in, JsonNode.class);
+            FLIGHT_INFO_FAID = mapper.createParser(jsonNode.toString()).readValueAs(FlightInfoFaid.class);
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
@@ -43,6 +60,17 @@ public class AeroApiClientCallerTest {
         FlightInfoFaid flightInfoFaId = aeroApiClientCaller.getFlightFromFaId(null);
 
         assertNull(flightInfoFaId);
+    }
+
+    @Test
+    public void getFlightFromFaIdShouldFlightInfoIdObject() {
+        when(client.exchange(anyString(), any(), any(), eq(JsonNode.class))).thenReturn(responseEntity);
+        when(responseEntity.getBody()).thenReturn(jsonNode);
+
+        FlightInfoFaid result = aeroApiClientCaller.getFlightFromFaId("AIC102");
+        result.setFullJson(null);
+
+        assertEquals(FLIGHT_INFO_FAID, result);
     }
 
     @Test
