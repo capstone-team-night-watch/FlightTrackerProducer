038efddf4e83ad0d8fc9fd52c1b4e851f706fd72
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2024-03-23 13:06:35 -0500

    created tests for AviationStackClientCaller

--- src/test/java/com/capstone/producer/clients/AviationStackClientCallerTest.java
+++ src/test/java/com/capstone/producer/clients/AviationStackClientCallerTest.java
@@ -1,8 +1,11 @@
 package com.capstone.producer.clients;
 
+import com.capstone.producer.common.bindings.aviationstack.AirportAviation;
 import com.capstone.producer.common.bindings.aviationstack.FlightInfo;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -10,36 +13,87 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AviationStackClientCallerTest {
-    private static final String BASE_URL = "http://localhost:8080/services";
+    private final String BASE_URL = "http://localhost:8080/services";
+    private static final String FLIGHT_INFO_DATA_JSON = "aviation_flight_info_data.json";
+    private static final String FLIGHTS_INFO_DATA_JSON = "aviation_flights_info_data.json";
+    private static final String AVIATION_AIRPORT_DATA_JSON = "aviation_airport_data.json";
+    private static final String AVIATION_AIRPORTS_DATA_JSON = "aviation_airports_data.json";
 
     @Mock
     private RestTemplate client;
+
     @Mock
     private ResponseEntity<JsonNode> responseEntity;
 
     private AviationStackClientCaller aviationStackClientCaller;
 
+    private FlightInfo flightInfo;
+    private JsonNode flightInfoData;
+    private JsonNode flightsInfoData;
+    private AirportAviation aviationAirport;
+    private JsonNode aviationAirportsData;
+
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
         aviationStackClientCaller = new AviationStackClientCaller(client, BASE_URL, "key");
+        flightInfo = readJson(FLIGHT_INFO_DATA_JSON, FlightInfo.class);
+        flightInfoData = readJsonNode(FLIGHT_INFO_DATA_JSON);
+        flightsInfoData = readJsonNode(FLIGHTS_INFO_DATA_JSON);
+        aviationAirport = readJson(AVIATION_AIRPORT_DATA_JSON, AirportAviation.class);
+        aviationAirportsData = readJsonNode(AVIATION_AIRPORTS_DATA_JSON);
     }
 
     @Test
-    public void shouldReturnEmptyListWithNullRootNode(){
-        when(client.exchange(anyString(), any(), any(), eq(JsonNode.class))).thenReturn(responseEntity);
-        when(responseEntity.getBody()).thenReturn(null);
-
-        List<FlightInfo> flightInfoList = aviationStackClientCaller.getAllActiveFlightsWithLive();
-
-        assertEquals(Collections.emptyList(), flightInfoList);
+    public void getAllActiveFlightsShouldReturnEmpty() {
+        mockResponse(null);
+        assertEquals(0, aviationStackClientCaller.getAllActiveFlightsWithLive().size());
     }
+
+    @Test
+    public void getAllActiveFlightsShouldReturnList() {
+        mockResponse(flightsInfoData);
+        assertEquals(5, aviationStackClientCaller.getAllActiveFlightsWithLive().size());
+    }
+
+    @Test
+    public void getAirportInfoFromNameShouldReturnNull() {
+        mockResponse(null);
+        assertNull(aviationStackClientCaller.getAirportInfoFromName(null));
+    }
+
+    @Test
+    public void getAirportInfoFromNameShouldReturnAirport() {
+        mockResponse(aviationAirportsData);
+        assertEquals(aviationAirport, aviationStackClientCaller.getAirportInfoFromName("Anaa"));
+    }
+
+    private <T> T readJson(String fileName, Class<T> valueType) throws IOException {
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(in, valueType);
+        }
+    }
+
+    private JsonNode readJsonNode(String fileName) throws IOException {
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readTree(in);
+        }
+    }
+
+    private void mockResponse(JsonNode body) {
+        when(client.exchange(anyString(), any(), any(), eq(JsonNode.class))).thenReturn(responseEntity);
+        when(responseEntity.getBody()).thenReturn(body);
+    }
+
 }
